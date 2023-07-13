d437faa302775f4ac712a017b45aafa9becb6701
molnargabo <139471980+molnargabo@users.noreply.github.com>
2023-07-13 15:50:52 +0200

    Change links

--- src/data/data.js
+++ src/data/data.js
@@ -32,44 +32,12 @@ const themeData = {
 const linkData = [
   {
     id: 1,
-    link: "https://snatchcasinos.com/d73b090f9",
-    name: "SNATCH",
-    description: "200% first depo bónusz, non-sticky, max co a depohoz képest 10x + 100 FS a Dog House sloton (40x wager 100 € max CO). Első 4 befizetésedre van bónusz.",
+    link: "https://bonafides.club/66389/790?l=1",
+    name: "FAIRSPIN",
+    description: "Heti cashback (5-50%), nyereményjátékok az oldalon + a regisztrációd mellé kapsz 200 TFS tokent. 200% non-sticky bónusz első befizetésre 60x wager (ami igazából 30x mert csak a bónuszösszegre vonatkozik). Nincs max bet, lehet buyolni. Ha elbuktad a befizetett összeget utána már csak a 'Play with bonus' menüpont alatt található slotokon tudsz játszani.",
     image:
-      "https://slotcatalog.com/userfiles/image/casino/Snatch-Casino_logo.jpg",
-  },
-   {
-    id: 2,
-    link: "https://vavadacompartner.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
-    name: "VAVADA",
-    description: "100% első befizetési bónusz 1000 €-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
-    image:
-      "https://www.deluxecasinobonus.net/images/Casinos/vavada/vavada-casino-logo.png",
-  },
-   {
-    id: 3,
-    link: "https://go.24slots.partners/visit/?bta=35327&nci=5543",
-    name: "24SLOTS",
-    description: "PROMO CODE: TAMBA. 200%-os befizetési bónusz 500€-ig, 35x wager, max bet: 12€",
-    image:
-      "https://cdn.sanity.io/images/710sk6qy/production/a5b5e2a3012d4a8badb854cd5a06efe76d021a48-270x270.png",
-  },
-  {
-    id: 4,
-    link: "https://media.kongaffiliates.com/redirect.aspx?pid=4404&lpid=13&bid=1485",
-    name: "DOLLY CASINO",
-    description: "100%-os első befizetési bónusz akár 150 ezer Ft-ig (NON-STICKY a bónusz, 35x wager)",
-    image:
-      "https://cdn.sanity.io/images/710sk6qy/production/0b36c73fe31527ab1d6f17d43550acc4325009dc-270x270.png",
-  },
-   {
-    id: 5,
-    link: "https://ads.lvbetpartners.com/redirect.aspx?pid=9570&bid=3838",
-    name: "LVBET",
-    description: "200%-os első befizetési bónusz (20x wager) 10 ezer Ft a bónusz maximális összege, tehát 5 ezer Ft befizetéssel maxolod ki a bónuszt (ADBLOCK nélkül nyitja meg a linket!)",
-    image:
-      "https://kaszinok.online/image/cache/data/1-svg-casino-logos/lvbet-1200x630sh.svg",
-  },
+      "https://th.bing.com/th/id/R.5828c5eb857467d834515b14a90f3222?rik=yx2ye51%2bT0VNGg&pid=ImgRaw&r=0",
+  }
 ];
 
 export { socialLinks, profileData, linkData, themeData };
