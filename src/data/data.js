38a2246f6909c7e4978b9984543077428a4e592f
molnargabo <139471980+molnargabo@users.noreply.github.com>
2023-07-26 19:11:10 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -32,12 +32,20 @@ const themeData = {
 const linkData = [
   {
     id: 1,
+    link: "https://legzocasino.life/c53c8eda1",
+    name: "LEGZO",
+    description: "Depo nélküli 50 Freespin (0.2€ x 50) a Candy Monsta (BGaming) slotra - Kód: GABO Megadod a telefon, e-mail, név aztán megerősíted a telószámod és az e-mailed és jogosult vagy a free spinekre. Nincs max CO wager: 45x A Bonus-money games kategóriában lévő slotokon tudod megforgatni a wagert. Depó bónusz wager: 20x, nincs bonus buy, max bet 5€. ",
+    image:
+      "https://www.spicycasinos.com/wp-content/uploads/2022/11/legzo_casino_logo_300x300_mini.jpg",
+  },
+  {
+    id: 2,
     link: "https://bonafides.club/66389/790?l=1",
     name: "FAIRSPIN",
     description: "Heti cashback (5-50%), nyereményjátékok az oldalon + a regisztrációd mellé kapsz 200 TFS tokent. 200% non-sticky bónusz első befizetésre 60x wager (ami igazából 30x mert csak a bónuszösszegre vonatkozik). Nincs max bet, lehet buyolni. Ha elbuktad a befizetett összeget utána már csak a 'Play with bonus' menüpont alatt található slotokon tudsz játszani.",
     image:
       "https://th.bing.com/th/id/R.5828c5eb857467d834515b14a90f3222?rik=yx2ye51%2bT0VNGg&pid=ImgRaw&r=0",
-  }
+  },
 ];
 
 export { socialLinks, profileData, linkData, themeData };
